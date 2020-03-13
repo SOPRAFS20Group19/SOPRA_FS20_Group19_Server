baf0919d32999321914136368cb17be5dc08445a
Luis Knufinke <luis.knufinke@uzh.ch>
2020-03-13 12:19:47 +0100

    initial deploy to heroku

--- build/generated/sources/annotationProcessor/java/main/ch/uzh/ifi/seal/soprafs20/rest/mapper/DTOMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/ch/uzh/ifi/seal/soprafs20/rest/mapper/DTOMapperImpl.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-03-06T14:49:16+0100",
+    date = "2020-03-13T12:14:13+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 13.0.1 (Oracle Corporation)"
 )
 public class DTOMapperImpl implements DTOMapper {
