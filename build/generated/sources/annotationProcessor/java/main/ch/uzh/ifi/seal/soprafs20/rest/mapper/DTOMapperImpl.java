be97bfe703b0aaecb898aca9b592417a1a7b2297
Luis Knufinke <luis.knufinke@uzh.ch>
2020-05-24 13:45:48 +0200

    final test report

--- build/generated/sources/annotationProcessor/java/main/ch/uzh/ifi/seal/soprafs20/rest/mapper/DTOMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/ch/uzh/ifi/seal/soprafs20/rest/mapper/DTOMapperImpl.java
@@ -16,8 +16,8 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-05-24T13:03:35+0200",
-    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 13.0.2 (Oracle Corporation)"
+    date = "2020-05-24T13:38:23+0200",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 13.0.1 (Oracle Corporation)"
 )
 public class DTOMapperImpl implements DTOMapper {
 
