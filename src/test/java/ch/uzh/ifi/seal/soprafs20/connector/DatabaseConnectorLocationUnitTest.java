a48fc085d2f201a84364fde882a2d08ddc9ccfda
maxachakri <maximilian.achakri@uzh.ch>
2020-05-24 13:04:18 +0200

    little update

--- src/test/java/ch/uzh/ifi/seal/soprafs20/connector/DatabaseConnectorLocationUnitTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/connector/DatabaseConnectorLocationUnitTest.java
@@ -11,36 +11,42 @@ import static org.junit.jupiter.api.Assertions.*;
 @WebAppConfiguration
 public class DatabaseConnectorLocationUnitTest {
 
+    //Test if the Id has the right range
     @Test
     public void generateFountainIdTest(){
         int id = DatabaseConnectorLocation.generateFountainId();
         assertTrue(id>=10000000 && id<20000000);
     }
 
+    //Test if the Id has the right range
     @Test
     public void generateRecyclingStationIdTest(){
         int id = DatabaseConnectorLocation.generateRecyclingId();
         assertTrue(id>=30000000 && id<40000000);
     }
 
+    //Test if the Id has the right range
     @Test
     public void generateFireplaceIdTest(){
         int id = DatabaseConnectorLocation.generateFireplaceId();
         assertTrue(id>=20000000 && id<30000000);
     }
 
+    //Test if the Id has the right range
     @Test
     public void generateToiletsIdTest(){
         int id = DatabaseConnectorLocation.generateToiletId();
         assertTrue(id>=40000000 && id<50000000);
     }
 
+    //Test if the Id has the right range
     @Test
     public void generateBenchIdTest(){
         int id = DatabaseConnectorLocation.generateBenchId();
         assertTrue(id>=60000000 && id<70000000);
     }
 
+    //Test if the Id has the right range
     @Test
     public void generateTableTennisIdTest(){
         int id = DatabaseConnectorLocation.generateTableTennisId();
